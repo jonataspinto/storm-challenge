2e158404f4934d9bbd112f0b54f8c524db695c6f
Jonatas Pinto <jonatastdb_jp@hotmail.com>
2019-12-10 14:48:26 -0300

    Updates

---
+++ precache-manifest.96122ce4dea1c77e2af8d0e426154cea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd72089df26af998738e8848fc6b3ce",
+    "url": "/storm-challenge/index.html"
+  },
+  {
+    "revision": "149da81f4a25499d3936",
+    "url": "/storm-challenge/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "079e445a3f642eacaa31",
+    "url": "/storm-challenge/static/js/2.e5fa7441.chunk.js"
+  },
+  {
+    "revision": "374a015f9bd7dbb4f757a46f1c6a7946",
+    "url": "/storm-challenge/static/js/2.e5fa7441.chunk.js.LICENSE"
+  },
+  {
+    "revision": "149da81f4a25499d3936",
+    "url": "/storm-challenge/static/js/main.41b44ae4.chunk.js"
+  },
+  {
+    "revision": "c548d067d47d19f34d84",
+    "url": "/storm-challenge/static/js/runtime-main.4bd73b9d.js"
+  }
+]);
